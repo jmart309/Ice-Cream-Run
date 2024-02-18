31bb0a9c794062b4ba46c1033ff758ffb9a5b89c
Jose Cuellar <jcuell2@uic.edu>
2024-02-17 20:12:41 -0600

    Set window size to 1024x576. Added PlayingScreen.java which contains the background map the game will be played on.

--- Coding Project/core/src/com/mygdx/game/TruckDriver.java
+++ Coding Project/core/src/com/mygdx/game/TruckDriver.java
@@ -15,23 +15,26 @@ public class TruckDriver {
         public boolean dispatchKeyEvent(KeyEvent e) {
             // Check if "W" key is pressed
             if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyChar() == 'W') {
-                // Move the truck to (200, 200)
+                // Move the truck upwards on W & decrease fuel as we move
                 truckX = 100;
                 truckY = 150;
+                fuel = fuel--;
                 System.out.println("Truck moved to (200, 200)");
             }
 
             if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyChar() == 'D') {
-                // Move the truck to (200, 200)
+                // Move the truck straight on D
                 truckX = 100;
                 truckY = 100;
+                fuel = fuel--;
                 System.out.println("Truck moved to (200, 200)");
             }
 
             if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyChar() == 'S') {
-                // Move the truck to (200, 200)
+                // Move the truck to Downwards with S
                 truckX = 100;
                 truckY = 50;
+                fuel = fuel--;
                 System.out.println("Truck moved to (200, 200)");
             }
             return false;
